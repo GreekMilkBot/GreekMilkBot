c39a571399fa7bc083387bbb0c19ac2f06c3426c
dragon <ExplodingFKL@gmail.com>
2025-05-23 17:32:09 +0800

    push some trash code

--- bot/route.go
+++ bot/route.go
@@ -1,25 +1,56 @@
 package bot
 
 import (
-	"container/list"
+	"errors"
+	"sync"
 	"time"
 )
 
+type PacketType int
+
 type Packet struct {
-	From string `json:"from"` // 来源 (必须是一个真实的注册位置)
-	To   string `json:"to"`   // 目标 (任意内容，消息可被丢弃)
+	ID    uint64   `json:"id"`    // 包的 id, 保证在每次会话中唯一
+	Stack []string `json:"stack"` // 包的路由信息,每次操作此数据包均需要附加之前的来源
 
-	Created time.Time `json:"created"` // 创建时间
+	Source   string    `json:"src"`    // 来源
+	Target   string    `json:"dest"`   // 目标
+	CreateAt time.Time `json:"create"` // 创建时间
 
-	Content string `json:"content"` // 消息内容
+	Content string `json:"data"` // 消息内容
 }
 
 type Router struct {
-	list *list.List
+	packets chan Packet
+	lock    *sync.RWMutex
 }
 
-func NewRouter() *Router {
+func NewRouter(cache int) *Router {
 	return &Router{
-		list: list.New(),
+		packets: make(chan Packet, cache),
+		lock:    &sync.RWMutex{},
 	}
+}
+
+type Client struct {
+	name   string
+	router *Router
+}
+
+func (r *Router) NewClient(name string) (*Client, error) {
+	if name == "" {
+		return nil, errors.New("empty name")
+	}
+	return &Client{
+		name:   name,
+		router: r,
+	}, nil
+}
+
+func (c *Client) Send(pType, target, msg string) error {
+
+	panic("implement me")
+}
+
+func (c *Client) RPC() <-chan Packet {
+
 }
